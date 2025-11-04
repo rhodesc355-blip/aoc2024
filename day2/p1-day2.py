decd66ad547a92777c6054f4c5dff8985f567833
rhodesc355-blip <rhodesc.355@gmail.com>
2025-11-04 18:27:38 +0000

    day2 - part1

---
+++ day2/p1-day2.py
@@ -0,0 +1,44 @@
+# day2 - part1
+
+# Functions
+
+# Return True if safe, else false
+# Not safe if diff between elements is 1 <= diff < 3
+# Only safe if all elements are always increasing or decreasing. Not both
+def checkSafe(arr):
+    safe = True
+    asc = False
+    desc = False
+
+    for j in range(0,len(line)-1):
+        x = int(line[j])
+        y = int(line[j+1])
+        
+        if (x < y):
+            asc = True
+        elif (x > y):
+            desc = True
+
+        z = x-y
+        
+        if (abs(z) > 3) or (abs(z) == 0):
+            safe = False
+            break
+        if not (asc^desc): # XOR
+            safe = False
+            break
+    return safe
+
+# Main Code
+# ---------------------------------------------
+with open('input.txt', 'r') as file:
+    nums = [line.split() for line in file]
+
+ans = []
+
+for line in nums:
+    safe = checkSafe(line)
+    ans.append(safe)
+
+print("Sum of safe: " + str(sum(ans)))
+    
